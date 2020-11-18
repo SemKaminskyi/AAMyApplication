5007f5c2fed345a7c96fae4b771bcf8649fb4a0b
SemKaminskyi <kaminskyisem@gmail.com>
2020-11-18 17:54:06 +0200

    finish design port and land

--- app/src/main/java/com/gmail/kaminskysem/aamyapplication/MainActivity.kt
+++ app/src/main/java/com/gmail/kaminskysem/aamyapplication/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.gmail.kaminskysem.aamyapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val movie: ImageView = findViewById(R.id.iv_main_movie1)
+        movie.setOnClickListener {
+                openDetailsdActivity ()
+        }
+    }
+
+    private fun openDetailsdActivity() {
+       val secondIntent=  Intent (this, MovieDetails::class.java)
+        startActivity(secondIntent)
     }
 }
---
+++ app/src/main/java/com/gmail/kaminskysem/aamyapplication/MovieDetails.kt
@@ -0,0 +1,47 @@
+package com.gmail.kaminskysem.aamyapplication
+
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import android.graphics.Shader.TileMode
+import android.os.Bundle
+import android.view.Window
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MovieDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+        val tvTitle = findViewById<TextView>(R.id.tv_md_name_movie)
+        val tvStoryLine = findViewById<TextView>(R.id.tv_md_title_storyline)
+        val tvCast = findViewById<TextView>(R.id.tv_md_cast)
+        val tvBack = findViewById<TextView>(R.id.tv_md_back)
+
+
+        tvBack.setOnClickListener {
+            openMainActivity()
+        }
+
+
+        useGradientToTV(tvTitle)
+        useGradientToTV(tvStoryLine)
+        useGradientToTV(tvCast)
+
+
+    }
+    private fun openMainActivity() {
+        val intentMain = Intent(this, MainActivity::class.java)
+        startActivity(intentMain)
+    }
+
+    private fun useGradientToTV(tv: TextView) {
+        val col1 = Color.parseColor("#ECECEC")
+        val col2 = Color.parseColor("#C4C4C4")
+        val col3 = Color.parseColor("#FFFFFF")
+        val textShader: Shader = LinearGradient(0f, 6f, 12f, 0f, intArrayOf(col1, col2, col3), floatArrayOf(0f, 1f,1f), TileMode.CLAMP)
+        tv.getPaint().setShader(textShader)
+    }
+}
